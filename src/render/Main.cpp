bb1f5fae2abae846616e762f00bfafd9dafcffc7
Curle <curle@gemwire.uk>
2022-07-03 15:14:34 +0100

    New trace sample with a complex scene

--- src/render/Main.cpp
+++ src/render/Main.cpp
@@ -11,6 +11,7 @@
 #define GEOMETRY_OPERATOR_OVERLOADS
 #include "render/Geometry.h"
 #include "render/Ray.h"
+#include "view/World.h"
 
 // The tolerance for comparisons.
 static constexpr double epsilon = 0.0001;
@@ -23,46 +24,57 @@ int main(int argc, char* argv[]) {
     (void) argc;
     (void) argv;
 
-    Point origin(0, 0, -5);
-    int wallZ = 10;
-    double wallSize = 7;
-    int canvasSize = 100;
-    double worldPixelSize = wallSize / canvasSize;
-    double halfWall = wallSize / 2;
+    World w;
 
-    Framebuffer frame(canvasSize, canvasSize);
-    Sphere s;
+    // Level Geometry
+    Sphere floor;
+    floor.transform = Matrix::scaling(10, 0.01, 10);
+    floor.material = Material();
+    floor.material.color = Color(1, 0.9, 0.9);
+    floor.material.specular = 0;
 
-    Material m;
-    m.color = { 1, 0.2, 1};
+    Sphere leftWall;
+    leftWall.transform = Matrix::translation(0, 0, 5) *
+            Matrix::rotation_y(-M_PI / 4) * Matrix::rotation_x(M_PI / 2) *
+            Matrix::scaling(10, 0.01, 10);
+    leftWall.material = floor.material;
 
-    s.material = m;
+    Sphere rightWall;
+    rightWall.transform = Matrix::translation(0, 0, 5) *
+            Matrix::rotation_y(M_PI / 4) * Matrix::rotation_x(M_PI / 2) *
+            Matrix::scaling(10, 0.01, 10);
+    rightWall.material = floor.material;
 
-    PointLight light { {-10, 10, -10}, Color(0, 0, 0) };
+    // Objects
 
-    for (int i = 0; i < canvasSize - 1; i++) {
-        double worldY = halfWall - worldPixelSize * i;
+    Sphere middle;
+    middle.transform = Matrix::translation(-0.5, 1, 0.5);
+    middle.material = Material();
+    middle.material.color = Color(0.1, 1, 0.5);
+    middle.material.diffuse = 0.7;
+    middle.material.specular = 0.3;
 
-        for (int j = 0; j < canvasSize - 1; j++) {
-            double worldX = halfWall - worldPixelSize * j;
+    Sphere right;
+    right.transform = Matrix::translation(1.5, 0.5, -0.5) * Matrix::scaling(0.5, 0.5, 0.5);
+    right.material = Material();
+    right.material.color = Color(0.5, 1, 0.1);
+    right.material.diffuse = 0.7;
+    right.material.specular = 0.3;
 
-            Point pos(worldX, worldY, wallZ);
-            Ray r(origin, Vector((pos - origin).normalize()));
+    Sphere left;
+    left.transform = Matrix::translation(-1.5, 0.33, -0.75) * Matrix::scaling(0.33, 0.33, 0.33);
+    left.material = Material();
+    left.material.color = Color(1, 0.8, 1);
+    left.material.diffuse = 0.7;
+    left.material.specular = 0.3;
 
-            Intersections xs = Ray::intersect(s, r);
-            Intersection hit = xs.hit();
+    w.lightSource = PointLight { { -10, 10, -10 }, { 1, 1, 1 } };
+    w.objects = { floor, rightWall, leftWall, middle, right, left };
 
-            if (!hit.isEmpty()) {
-                Point hitPos = Ray::position(r, hit.time);
-                Vector hitNormal = hit.object.normalAt(hitPos);
-                Vector eyeDir = -r.direction;
+    Camera cam(100, 50, M_PI / 3);
+    cam.transform = World::viewMatrix({ 0, 1.5, -5 }, { 0, 1, 0 }, { 0, 1, 0 });
 
-                Color canvasColor = Light::lighting(m, light, hitPos, eyeDir, hitNormal);
-
-                frame.set(j, i, canvasColor);
-            }
-        }
-    }
+    Framebuffer frame = w.render(cam);
 
     std::ofstream out("pic.ppm");
     std::string text = frame.export_ppm();
